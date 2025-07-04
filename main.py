e8c80670af8928655a34a65092e6516c242833c4
Tabea Erkelenz <Tabea-erkelenz@t-online.de>
2025-07-04 10:11:16 +0200

    update main.py: so create_database gets called everytime main.py runs to create db if its not alredy done

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import movie_storage_sql as storage
 def main():
     """Entry point of the program. Initializes the movie database and starts the menu loop."""
     print(colors['title'] + "********** My Movies Database **********" + "\n")
+    create_database()
+
     movies = storage.list_movies()
     choose_menu(movies)
 
